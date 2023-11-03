1cd0fa9792aa3d86cebe4851d65cc7128f07888e
Tyler McWilliam <tyler.p.mcwilliam@altria.com>
2023-11-03 15:52:27 +0000

    Update App.js and challenge.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-// at the top of our file, we are going to be importing open source packages, and other files. 
+// at the top of our file, we are going to be importing open source packages, and other files.
 // for this file, we want to add in CSS, just like we would with a normal HTML File.
-import './appStyles.css';
-import { ThemeProvider, Typography } from '@mui/material';
-import theme from './config/theme';
-import Form from './components/form';
-import Table from './components/table';
-import Sidebar from './components/sidebar';
+import "./appStyles.css";
+import { ThemeProvider, Typography } from "@mui/material";
+import theme from "./config/theme";
+import Form from "./components/form";
+import Table from "./components/table";
+import Sidebar from "./components/sidebar";
+import data from "./data/tst.json";
 // Look at this! A function! We know functions.
 
-// This function actually returns HTML, which React handles perfectly. We can do cool things within 
-// this function that would be very clunky in a normal HTML page. 
+// This function actually returns HTML, which React handles perfectly. We can do cool things within
+// this function that would be very clunky in a normal HTML page.
 function App() {
   // with every function, we need to return something! That something could null, but that's a story for another day
+  console.log(data);
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Typography>Hello Web Cohort! This is an unstyled page with just some text inside</Typography>
+        <Typography>
+          Hello Web Cohort! This is an unstyled page with just some text inside
+        </Typography>
       </div>
     </ThemeProvider>
   );
 }
 
-
-// CHECK THIS OUT! So we have the function, but we need to tell JavaScript to make the function exportable so 
+// CHECK THIS OUT! So we have the function, but we need to tell JavaScript to make the function exportable so
 // that we can import this function into another part of the application.
 export default App;
--- src/pages/challenge.js
+++ src/pages/challenge.js
@@ -1,35 +1,33 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import GroupAddIcon from '@mui/icons-material/GroupAdd';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import theme from '.././config/theme';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import theme from ".././config/theme";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import { ThemeProvider } from "@mui/material/styles";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
+// You will want to start taking apart the tst.json data here! What will const cards need to be?
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function Challenge() {
@@ -45,12 +43,10 @@ export default function Challenge() {
         </Toolbar>
       </AppBar>
       <main>
-
-
         {/* Hero unit */}
         <Box
           sx={{
-            bgcolor: 'background.paper',
+            bgcolor: "background.paper",
             pt: 8,
             pb: 6,
           }}
@@ -65,32 +61,39 @@ export default function Challenge() {
             >
               Album layout
             </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Take this template, and reconfigure it to dynamically render media cards based on the UNIQUE brand names found in the data!
-              Challenges:
-              1. Render a unique media card for each brand name
-              2. Populate each media card with the total sales for that brand
-              3. Add the company logo to the media card
-              4. Use the rest of this page to figure out how to add a new MUI icon into your media card
+            <Typography
+              variant="h5"
+              align="center"
+              color="text.secondary"
+              paragraph
+            >
+              Take this template, and reconfigure it to dynamically render media
+              cards based on the SKUName found in the data! Challenges: 1.
+              Render a unique media card for each SKUName 2. Populate each media
+              card with the Brand and SKUGUID 4. Add a new MUI icon into your
+              media card 5. Still have time? Get creative!
             </Typography>
           </Container>
         </Box>
         <Container sx={{ py: 8 }} maxWidth="md">
           {/* End hero unit */}
 
-
           {/* You shouldn't need to make any changes to the grid!*/}
           <Grid container spacing={4}>
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
                 >
                   <CardMedia
                     component="div"
                     sx={{
                       // 16:9
-                      pt: '56.25%',
+                      pt: "56.25%",
                     }}
                     image="https://source.unsplash.com/random?wallpapers"
                   />
@@ -99,8 +102,8 @@ export default function Challenge() {
                       Heading
                     </Typography>
                     <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
+                      This is a media card. You can use this section to describe
+                      the content.
                     </Typography>
                   </CardContent>
                 </Card>
@@ -110,9 +113,8 @@ export default function Challenge() {
         </Container>
       </main>
 
-
       {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
         <Typography variant="h6" align="center" gutterBottom>
           Footer
         </Typography>
@@ -127,8 +129,6 @@ export default function Challenge() {
         <Copyright />
       </Box>
       {/* End footer */}
-
-
     </ThemeProvider>
   );
 }
